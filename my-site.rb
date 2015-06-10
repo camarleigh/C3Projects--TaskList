ce84c83d399f958941fc636112b3d29e95d63b09
marleigh <camarleigh@gmail.com>
2015-06-10 16:24:53 -0700

    created erb and html files

--- my-site.rb
+++ my-site.rb
@@ -6,8 +6,16 @@ require "sqlite3"
 class MySite < Sinatra::Base
   register Sinatra::Reloader
 
-  # get '/' do
-  #   erb :index
-  # end
+  get '/' do
+    erb :home
+  end
 
+  get '/taskform' do
+    erb :taskform
+  end
+
+  # post'/taskform' do
+  #   erb :taskform
+  # end
+  
 end
