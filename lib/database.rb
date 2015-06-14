557406a5d91a252f5f5bbcf76edd6d6db03e69d0
Loraine <loraine@lorainekv.com>
2015-06-13 19:07:08 -0700

    Added schema photo, required task name field, breakfast reqs completed.

--- lib/database.rb
+++ lib/database.rb
@@ -14,15 +14,14 @@ module TaskList
     def query!(statement, *params)
       db = SQLite3::Database.new @database_name
       db.execute statement, params
+
     rescue SQLite3::Exception => error
-      if params[:taskname] == "" || nil
-        puts "That's an error!"
-      end
+      puts "That's an error!"
 
       # use this block to recover from an error
       # consider giving the user a special message back
       # inspect the `error` object for information about the error
-      
+
     ensure
       db.close if db
     end
